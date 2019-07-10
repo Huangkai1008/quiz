3493159d1962233694660470e64c0bc23c98d631
huangkai <18778335525@163.com>
2019-07-10 20:51:35 +0800

    测试api ping

--- quiz/api/index.py
+++ quiz/api/index.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
 
 bp = Blueprint('index', __name__, url_prefix='/')
+
+
+@bp.route('/ping', methods=['GET'])
+def ping():
+    return dict(name='quiz', using='flask', description='Quiz is a Online q&a platform with Flask')
