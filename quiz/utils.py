54855ca1d84fc28537d0ff049278798f94beb272
huangkai <18778335525@163.com>
2019-07-13 19:20:30 +0800

    添加pyjwt和jwt工具类

--- quiz/utils.py
+++ quiz/utils.py
@@ -3,6 +3,8 @@ import decimal
 import json
 from collections import Iterator
 
+import jwt
+
 
 class ExtendedEncoder(json.JSONEncoder):
     def default(self, o):
@@ -15,3 +17,26 @@ class ExtendedEncoder(json.JSONEncoder):
         elif isinstance(o, Iterator):
             return list(o)
         return json.JSONEncoder.default(self, o)
+
+
+class Jwt:
+    """
+    jwt 工具类
+    """
+
+    def __init__(self, secret_key):
+        self._secret_key = secret_key
+
+    def generate_confirm_jwt(self, user_id):
+        """生成确认账户的Jwt"""
+        now = dt.datetime.utcnow()
+
+        payload = dict(
+            confirm=user_id,
+            exp=now + dt.timedelta(seconds=60 * 60),
+            iat=now,
+            aud='quiz'
+        )
+
+        token = jwt.encode(payload, self._secret_key, 'HS256').decode('utf-8')
+        return token
