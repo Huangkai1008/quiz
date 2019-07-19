4896c7d8a23a4c095cd7597fad40edc647d24ac4
huangkai <18778335525@163.com>
2019-07-19 22:27:59 +0800

    环境变量值类型转换

--- quiz/config/default.py
+++ quiz/config/default.py
@@ -9,13 +9,13 @@ DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 # ================ sqlalchemy ================
-SQLALCHEMY_POOL_SIZE = os.environ.get('SQLALCHEMY_POOL_SIZE')
+SQLALCHEMY_POOL_SIZE = int(os.environ.get('SQLALCHEMY_POOL_SIZE'))
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 SQLALCHEMY_RECORD_QUERIES = os.environ.get('SQLALCHEMY_RECORD_QUERIES')
 
 # ================ flask_mail ================
 MAIL_SERVER = os.environ.get('MAIL_SERVER')
-MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_PORT = int(os.environ.get('MAIL_PORT'))
 MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
