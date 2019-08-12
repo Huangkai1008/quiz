8181eef93defaa3236ed35983b57d8fe1a6631eb
huangkai <18778335525@163.com>
2019-08-12 23:04:17 +0800

    增加文章/文章主题/文章点赞数据库模型

--- quiz/model/__init__.py
+++ quiz/model/__init__.py
@@ -1,2 +1,3 @@
 from .user import User
 from .question import Question, Answer, AnswerVote
+from .article import Article, ArticleVote, Topic
---
+++ quiz/model/article.py
@@ -0,0 +1,43 @@
+import datetime as dt
+
+from sqlalchemy import PrimaryKeyConstraint
+
+from quiz.extensions import db
+from quiz.model.base import Model
+
+__all__ = ['Article', 'Topic', 'ArticleVote']
+
+
+class Article(Model):
+    """
+    文章
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, index=True, comment='文章作者id')
+    title = db.Column(db.String(128), comment='文章标题')
+    content = db.Column(db.String(2048), comment='文章内容')
+    create_time = db.Column(db.DateTime, default=dt.datetime.now(), comment='创建时间')
+    topic_id = db.Column(db.Integer, index=True, comment='主题/专题id')
+
+
+class Topic(Model):
+    """
+    主题/专题
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, index=True, comment='文章作者id')
+    topic_name = db.Column(db.String(54), comment='专题名')
+    topic_desc = db.Column(db.String(128), comment='专题描述')
+    create_time = db.Column(db.DateTime, default=dt.datetime.now(), comment='创建时间')
+
+
+class ArticleVote(Model):
+    """
+    文章点赞, 只有点赞状态
+    """
+    __table_args__ = (
+        PrimaryKeyConstraint('article_id', 'user_id'),
+    )
+
+    user_id = db.Column(db.Integer, index=True, comment='点赞此文章的用户id')
+    article_id = db.Column(db.Integer, index=True, comment='文章id')
