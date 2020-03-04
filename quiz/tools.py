bbe559517f08e2ea5a159a330fea57d89d7c31ed
huangkai <18778335525@163.com>
2020-03-04 22:16:04 +0800

    fix: 修复自定义flask返回list时的错误

--- quiz/tools.py
+++ quiz/tools.py
@@ -1,14 +1,15 @@
+import json
+
 from collections.abc import Iterator
 
 from flask import Flask as _Flask
+from flask import Response, current_app
 
 from quiz import utils
 
 
 class QuizFlask(_Flask):
-    """
-    自定义flask
-    """
+    """自定义flask"""
 
     json_encoder = utils.ExtendedEncoder
 
@@ -16,7 +17,10 @@ class QuizFlask(_Flask):
         if rv is None:
             rv = dict()
 
-        if isinstance(rv, Iterator):
-            rv = list(rv)
+        if isinstance(rv, (list, Iterator)):
+            return Response(
+                json.dumps(list(rv), cls=utils.ExtendedEncoder),
+                mimetype=current_app.config['JSONIFY_MIMETYPE'],
+            )
 
-        return super(QuizFlask, self).make_response(rv)
+        return super().make_response(rv)
